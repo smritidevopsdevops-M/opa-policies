bcd09ea4f375d89346a86dbfbbd210e727d145ec
unknown <smriti.pandey@abhiparihar15outlook.onmicrosoft.com>
2026-06-03 19:48:59 +0530

    blob file changed

--- Environments/test/module/blob-container/main.tf
+++ Environments/test/module/blob-container/main.tf
@@ -1,6 +1,5 @@
 resource "azurerm_storage_container" "this" {
-  name                  = var.container_name
-  #storage_account_id    = var.storage_account_id
-  storage_account_name  = var.storage_account_name
+  name               = var.container_name
+  storage_account_id = var.storage_account_id
   container_access_type = "private"
 }
--- Environments/test/module/blob-container/output.tf
+++ Environments/test/module/blob-container/output.tf
@@ -2,6 +2,7 @@ output "container_name" {
   value = azurerm_storage_container.this.name
 }
 
+
 output "container_id" {
   value = azurerm_storage_container.this.id
 }
